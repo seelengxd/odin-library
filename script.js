0592f9a22c2a0d84506fbe2ec163a539cab356c8
seeleng <seelengxd@gmail.com>
2021-12-20 22:29:22 +0800

    Refactored to use classes
    https://www.theodinproject.com/paths/full-stack-ruby-on-rails/courses/javascript/lessons/classes

--- script.js
+++ script.js
@@ -1,65 +1,79 @@
-let myLibrary = []
+class Book {
+  constructor(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
-function Book(title, author, pages, read){
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = () => `${this.title} by ${this.author}, ${this.read} pages, ${this.read ? 'already read' : 'not read yet'}`;
+  info(){
+    return `${this.title} by ${this.author}, ${this.read} pages, ${this.read ? 'already read' : 'not read yet'}`;
+  }
+
+  toggleReadStatus(){
+   this.read = !this.read;
+  }
 }
 
-Book.prototype.toggleReadStatus = function() {this.read = !this.read}
+class Library {
+  constructor(){
+    this.myLibrary = []
+  }
 
-function addBookToLibrary(title, author, pages, read){
-  newBook = new Book(title, author, pages, read)
-  myLibrary.push(newBook);
-  return newBook
-}
+  addBookToLibrary(title, author, pages, read){
+    let newBook = new Book(title, author, pages, read)
+    this.myLibrary.push(newBook);
+    return newBook
+  }
 
-function makeBookCard(book){
-  let container = document.querySelector(".container");
-  let card = document.createElement('div');
-  card.classList.add('card');
-  let title = document.createElement('h2');
-  title.textContent = book.title;
-  let author = document.createElement('p');
-  author.textContent = book.author;
-  let pages = document.createElement('p')
-  pages.textContent = book.pages + ' Pages';
-  let readButton = document.createElement('button')
-  readButton.textContent = book.read ? 'Already read' : 'Not read yet'
-  readButton.addEventListener('click', () => {
-    book.toggleReadStatus();
+  makeBookCard(book){
+    let container = document.querySelector(".container");
+    let card = document.createElement('div');
+    card.classList.add('card');
+    let title = document.createElement('h2');
+    title.textContent = book.title;
+    let author = document.createElement('p');
+    author.textContent = book.author;
+    let pages = document.createElement('p')
+    pages.textContent = book.pages + ' Pages';
+    let readButton = document.createElement('button')
     readButton.textContent = book.read ? 'Already read' : 'Not read yet'
-  })
-  let removeButton = document.createElement('button');
-  removeButton.textContent = 'Remove'
-  removeButton.addEventListener('click', () => {
-    myLibrary = myLibrary.filter((b) =>  b != book)
-    container.removeChild(card);
+    readButton.addEventListener('click', () => {
+      book.toggleReadStatus();
+      readButton.textContent = book.read ? 'Already read' : 'Not read yet'
+    })
+    let removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove'
+    removeButton.addEventListener('click', () => {
+      this.myLibrary = this.myLibrary.filter((b) =>  b != book)
+      container.removeChild(card);
+  
+    })
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages)
+    card.appendChild(readButton);
+    card.appendChild(removeButton);
+    return card;
+  }
 
-  })
-  card.appendChild(title);
-  card.appendChild(author);
-  card.appendChild(pages)
-  card.appendChild(readButton);
-  card.appendChild(removeButton);
-  return card
-}
-
-function display(book){
-  let container = document.querySelector(".container");
-  container.appendChild(makeBookCard(book));
-}
-function displayAllBooks(){
-  for (const book of myLibrary){
-    display(book)
+  display(book){
+    let container = document.querySelector(".container");
+    container.appendChild(this.makeBookCard(book));
+  }
+  
+  displayAllBooks(){
+    for (const book of this.myLibrary){
+      this.display(book)
+    }
   }
 }
 
 
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
-displayAllBooks();
+let library = new Library();
+
+library.addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+library.displayAllBooks();
 
 document.querySelector("#new").addEventListener('click', () => {
   document.querySelector('form').classList.toggle('hidden')
@@ -71,8 +85,8 @@ function handleForm(e){
   let author = document.querySelector("input[name='author']").value
   let pages = document.querySelector("input[name='pages']").value
   let read = document.querySelector("select[name='read']").value
-  let book = addBookToLibrary(title, author, pages, read == 'yes');
-  display(book);
+  let book = library.addBookToLibrary(title, author, pages, read == 'yes');
+  library.display(book);
 }
 
 document.querySelector('form').onsubmit = handleForm
